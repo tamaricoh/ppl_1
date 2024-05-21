cf30aaeb2ae7b0c7b23886545a79dd71b032e80a
tamaricoh <106402042+tamaricoh@users.noreply.github.com>
2024-05-21 11:11:20 +0300

    2.1

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -3,16 +3,27 @@ import * as R from "ramda";
 const stringToArray = R.split("");
 
 /* Question 1 */
-export const countVowels: undefined = undefined
+const isVow: (ch: string) => boolean = (ch) => {
+  // const arr = stringToArray("AEIOUaeiou");
+  return R.includes(ch, "AEIOUaeiou");
+};
+
+export const countVowels: (str: string) => number = (str) => {
+  const arr = stringToArray(str);
+  const f = R.pipe(
+    R.filter(isVow),
+    R.reduce((acc, _) => acc + 1, 0)
+  );
+  return f(arr);
+};
 
 /* Question 2 */
-export const isPaired: undefined = undefined
+export const isPaired: undefined = undefined;
 
 /* Question 3 */
 export type WordTree = {
-    root: string;
-    children: WordTree[];
-}
+  root: string;
+  children: WordTree[];
+};
 
-export const treeToSentence : undefined = undefined
-
+export const treeToSentence: undefined = undefined;
