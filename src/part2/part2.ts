b0c53f446588f7e85117fa65624bd0469d9cfc32
tamaricoh <106402042+tamaricoh@users.noreply.github.com>
2024-05-21 11:39:58 +0300

    2.2

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -18,7 +18,20 @@ export const countVowels: (str: string) => number = (str) => {
 };
 
 /* Question 2 */
-export const isPaired: undefined = undefined;
+const katlan: (num: number, ch: string) => number = (num, ch) => {
+  return R.includes(ch, "({[") ? num + 1 : num > 0 ? num - 1 : 0.25;
+};
+export const isPaired: (str: string) => boolean = (str) => {
+  const arr = stringToArray(str);
+  const type1 = R.filter((ch) => R.includes(ch, "[]"), arr);
+  const type2 = R.filter((ch) => R.includes(ch, "()"), arr);
+  const type3 = R.filter((ch) => R.includes(ch, "{}"), arr);
+  return (
+    R.reduce(katlan, 0, type1) === 0 &&
+    R.reduce(katlan, 0, type2) === 0 &&
+    R.reduce(katlan, 0, type3) === 0
+  );
+};
 
 /* Question 3 */
 export type WordTree = {
